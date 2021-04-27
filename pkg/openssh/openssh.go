90dc7a984c0fd9ca814dd8fbf2a9dbf45f74a2e8
Sören Gürtler <soeren@guertler.net>
2021-04-27 10:25:40 +0200

    add password param to keyfile parsing Allows a password to be passed for opening a OpenSSH keyfile. Despite that opening encrypted keyfiles is not supported, so effectively that value is ignored.

--- pkg/openssh/openssh.go
+++ pkg/openssh/openssh.go
@@ -4,32 +4,31 @@ import (
 	"bytes"
 	"crypto/ed25519"
 	"encoding/binary"
-	"encoding/hex"
 	"encoding/pem"
 	"fmt"
 	"io/ioutil"
 	"log"
 )
 
-func FromFile(filename string) (ed25519.PrivateKey, error) {
+func FromFile(filename, password string) (ed25519.PrivateKey, error) {
 	pemBytes, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
 
-	return FromPem(pemBytes)
+	return FromPem(pemBytes, password)
 }
 
-func FromPem(pemBytes []byte) (ed25519.PrivateKey, error) {
+func FromPem(pemBytes []byte, password string) (ed25519.PrivateKey, error) {
 	block, _ := pem.Decode(pemBytes)
 	if block == nil {
 		return nil, fmt.Errorf("unable to get pem block")
 	}
 
-	return FromBytes(block.Bytes)
+	return FromBytes(block.Bytes, password)
 }
 
-func FromBytes(raw []byte) (ed25519.PrivateKey, error) {
+func FromBytes(raw []byte, password string) (ed25519.PrivateKey, error) {
 	magicNullterm := bytes.Index(raw, []byte{0x00})
 	authMagic := string(raw[:magicNullterm])
 	raw = raw[magicNullterm+1:]
@@ -38,19 +37,11 @@ func FromBytes(raw []byte) (ed25519.PrivateKey, error) {
 	}
 
 	raw, ciphername := readNextString(raw)
-	_ = ciphername
 	raw, kdfname := readNextString(raw)
 	raw, kdfopts := readNextBytes(raw)
 	raw, num := readNextInt(raw)
 	if num != 0x01 {
 		return nil, fmt.Errorf("invalid value for number of keys %d", num)
-	}
-	
-	if kdfname == "bcrypt" && len(kdfopts) == 24 {
-		foo, salt := readNextBytes(kdfopts)
-		log.Println(hex.EncodeToString(salt))
-		_, work := readNextInt(foo)
-		log.Println(work)
 	}
 
 	//Skip Public-Key length
@@ -64,14 +55,31 @@ func FromBytes(raw []byte) (ed25519.PrivateKey, error) {
 
 	if ciphername == "none" && kdfname == "none" {
 		return keyFromPayload(raw[:payloadSize])
+	}else if kdfname == "bcrypt" {
+		/*
+		For reference:
+		https://peterlyons.com/problog/2017/12/openssh-ed25519-private-key-file-format/
+		https://crypto.stackexchange.com/questions/58536/how-does-openssh-use-bcrypt-to-set-ivs
+		https://cvsweb.openbsd.org/cgi-bin/cvsweb/src/usr.bin/ssh/PROTOCOL.key?annotate=HEAD
+		https://cvsweb.openbsd.org/cgi-bin/cvsweb/src/usr.bin/ssh/sshkey.c?rev=1.64&content-type=text/x-cvsweb-markup&only_with_tag=MAIN
+		*/
+
+		salt := kdfopts[4:20]
+		rounds := kdfopts[20:24]
+		log.Println(salt)
+		log.Println(rounds)
+		return nil, fmt.Errorf("encrypted keys are currently not supported")
 	}else {
-		return nil, nil
+		return nil, fmt.Errorf("unable to decrypt cipher %s with kdf %s", ciphername, kdfname)
 	}
 }
 
 func keyFromPayload(payload []byte) (ed25519.PrivateKey, error) {
-	//Skip weird 8 bytes
-	payload = payload[8:]
+	payload, checkint1 := readNextInt(payload)
+	payload, checkint2 := readNextInt(payload)
+	if checkint1 != checkint2 {
+		return nil, fmt.Errorf("checkints don't match (key probably wrong)")
+	}
 
 	payload, keytype := readNextString(payload)
 	if keytype != "ssh-ed25519" {
