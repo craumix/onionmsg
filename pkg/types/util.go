b013e85494ae41d27da3231242b36e210e1caf45
Sören Gürtler <soeren@guertler.net>
2021-05-04 21:24:38 +0200

    add contactrequest -/response structs

--- pkg/types/util.go
+++ pkg/types/util.go
@@ -3,7 +3,20 @@ package types
 import (
 	"crypto/rand"
 	"encoding/base64"
+
+	"github.com/google/uuid"
 )
+
+type ContactRequest struct {
+	RemoteFP string
+	LocalFP  string
+	ID       uuid.UUID
+}
+
+type ContactResponse struct {
+	ConvFP string
+	Sig    []byte
+}
 
 func RandomString(size int) string {
 	r := make([]byte, size)
