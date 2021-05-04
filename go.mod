b49f07525657fbc5d0833167561db61de95250b7
Sören Gürtler <soeren@guertler.net>
2021-05-04 21:51:32 +0200

    remove identity qr code generation from backend

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.16
 require (
 	github.com/google/uuid v1.2.0
 	github.com/klauspost/compress v1.12.2
-	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 	github.com/wybiral/torgo v0.0.0-20201209223426-5fd9910eab31
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/net v0.0.0-20210423184538-5f58ad60dda6
--- pkg/types/identity.go
+++ pkg/types/identity.go
@@ -1,13 +1,8 @@
 package types
 
 import (
-	"bytes"
 	"crypto/ed25519"
 	"encoding/base64"
-	"image"
-	"image/png"
-
-	qrcode "github.com/skip2/go-qrcode"
 )
 
 type Identity struct {
@@ -26,20 +21,6 @@ func NewIdentity() *Identity {
 
 func (i *Identity) Fingerprint() string {
 	return i.B64PubKey()
-}
-
-func (i *Identity) QR(res int) (image.Image, error) {
-	b, err := qrcode.Encode(i.Fingerprint(), qrcode.Medium, res)
-	if err != nil {
-		return nil, err
-	}
-
-	img, err := png.Decode(bytes.NewReader(b))
-	if err != nil {
-		return nil, err
-	}
-
-	return img, nil
 }
 
 func (i *Identity) B64PubKey() string {
