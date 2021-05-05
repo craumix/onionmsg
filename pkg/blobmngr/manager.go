7a681d131f97e334fd35efe3642c88312204d01c
Sören Gürtler <soeren@guertler.net>
2021-05-05 23:29:25 +0200

    make blobmanager able to stream from blob to writer

--- pkg/blobmngr/manager.go
+++ pkg/blobmngr/manager.go
@@ -1,6 +1,7 @@
 package blobmngr
 
 import (
+	"io"
 	"io/ioutil"
 	"os"
 
@@ -23,6 +24,28 @@ func Initialize(dir string) error {
 
 func GetRessource(id uuid.UUID) ([]byte, error) {
 	return ioutil.ReadFile(resolveResPath(id))
+}
+
+func Stream(id uuid.UUID, w io.Writer) error {
+	file, err := os.OpenFile(resolveResPath(id), os.O_RDONLY, 0600)
+	if err != nil {
+		return err
+	}
+
+	buf := make([]byte, 4096)
+	for n, err := file.Read(buf); n > 0 && err != nil; {
+		_, err := w.Write(buf[:n])
+		if err != nil {
+			return err
+		}
+	}
+	if err != nil {
+		return err
+	}
+
+	file.Close()
+	
+	return nil
 }
 
 func SaveRessource(blob []byte) (uuid.UUID, error) {
