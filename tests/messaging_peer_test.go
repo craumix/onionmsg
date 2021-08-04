69e9bc1b875ca1ff2cffa6086706778307f35b16
Merlin Volkmer <merlin.volkmer@outlook.com>
2021-08-04 18:10:03 +0200

    Add simple test for MessagingPeer

---
+++ tests/messaging_peer_test.go
@@ -0,0 +1,46 @@
+package tests
+
+import (
+	"github.com/craumix/onionmsg/pkg/types"
+	"os"
+	"testing"
+	"time"
+)
+
+var (
+	peer    *types.MessagingPeer
+	message types.Message
+)
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	os.Exit(code)
+}
+
+func setup() {
+	identity, _ := types.NewRemoteIdentity("Test")
+	peer = types.NewMessagingPeer(identity)
+
+	message = types.Message{
+		Meta: types.MessageMeta{
+			Sender: "test",
+			Time:   time.Time{},
+			Type:   "mtype.text",
+		},
+		Content:     []byte("this is a test"),
+		ContentMeta: nil,
+	}
+}
+
+func TestQueueMessage(t *testing.T) {
+	if len(peer.MQueue) != 0 {
+		t.Error("Peer doesn't start with an empty Message queue")
+	}
+
+	peer.QueueMessage(message)
+
+	if len(peer.MQueue) != 1 {
+		t.Error("Message not queued")
+	}
+}
